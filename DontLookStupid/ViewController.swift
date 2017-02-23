b91abe84be24e57bbbab5b879dcbfe14b3f13be9
klicker22 <aklick_22@hotmail.com>
2017-02-22 19:20:18 -0600

    Camera access and app icon added

--- DontLookStupid/ViewController.swift
+++ DontLookStupid/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreLocation
+import AVFoundation
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
 
@@ -15,25 +16,71 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     
     @IBOutlet weak var speedLabel: UILabel!
+    @IBOutlet weak var frontCameraView: UIView!
+    @IBOutlet weak var picTakenLabel: UILabel!
     
     
+    let captureSession = AVCaptureSession()
+    let sessionOutput = AVCaptureVideoDataOutput()
+    var previewLayer = AVCaptureVideoPreviewLayer()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        let deviceSession = AVCaptureDeviceDiscoverySession(deviceTypes: [.builtInDualCamera, .builtInTelephotoCamera, .builtInWideAngleCamera], mediaType: AVMediaTypeVideo, position: .unspecified)
+        
+        for device in (deviceSession?.devices)! {
+            
+            if device.position == AVCaptureDevicePosition.front {
+                
+                do {
+                    
+                    let input = try AVCaptureDeviceInput(device: device)
+                    
+                    if captureSession.canAddInput(input) {
+                        captureSession.addInput(input)
+                        
+                        if captureSession.canAddOutput(sessionOutput) {
+                            captureSession.addOutput(sessionOutput)
+                            
+                            previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+                            previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+                            previewLayer.connection.videoOrientation = .portrait
+                            
+                            frontCameraView.layer.addSublayer(previewLayer)
+                            frontCameraView.addSubview(speedLabel)
+                            frontCameraView.addSubview(picTakenLabel)
+                            
+                            previewLayer.position = CGPoint(x: self.frontCameraView.frame.width / 2, y: self.frontCameraView.frame.height / 2)
+                            previewLayer.bounds = frontCameraView.frame
+                            
+                            captureSession.startRunning()
+                            
+                        }
+                        
+                    }
+                    
+                } catch let avError {
+                    print(avError)
+                }
+                
+            }
+            
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if CLLocationManager.locationServicesEnabled() {
             
-            
             locationManager = CLLocationManager()
             locationManager.delegate = self
             locationManager.requestAlwaysAuthorization()
             locationManager.requestWhenInUseAuthorization()
             locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
-            
             locationManager.startUpdatingLocation()
             
         }
-        
         
     }
 
@@ -42,24 +89,32 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         
-        
         let speed = (((manager.location!.speed) * 2.23694) * 100).rounded() / 100
+        
         //let location = locations[0]
-        
-        
         //let locValue: CLLocationCoordinate2D = manager.location!.coordinate
-        
         //speedLabel.text = "Latitude = \(location.coordinate.latitude)\n Longitude = \(location.coordinate.longitude)"
         
         if speed < 0 {
-            
             speedLabel.text = "0.0 MPH"
-            
         } else {
             speedLabel.text = "\(speed) MPH"
         }
     }
 
+    
+//    @IBAction func librarySwipe(_ sender: Any) {
+//        
+//        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.photoLibrary) {
+//            var imagePicker = UIImagePickerController()
+//            imagePicker.delegate = self
+//            imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+//            imagePicker.allowsEditing = true
+//            self.presentedViewController(imagePicker, animated: true, completion: nil)
+//            
+//        }
+//        
+//    }
 
 }
 
